49cd20e8bc1bed3ee6e0825bd9da53d8605d287f
Angela Nicholas <anicholas@walmartlabs.com>
2015-02-13 14:16:33 -0800

    add button examples to style guide

--- examples/app.jsx
+++ examples/app.jsx
@@ -12,16 +12,22 @@ var Form = require('../modules/components/form.jsx');
 var FormGroup = require('../modules/components/form-group.jsx');
 var Label = require('../modules/components/label.jsx');
 var Input = require('../modules/components/input.jsx');
+var Button = require('../modules/components/button.jsx');
 
-var basicForm = require('raw!./../examples/components/basic-form.txt');
-var inlineForm = require('raw!./../examples/components/inline-form.txt');
-var horizontalForm = require('raw!./../examples/components/horizontal-form.txt');
 var fluidContainer = require('raw!./../examples/components/fluid-container.txt');
 var responsiveColumn = require('raw!./../examples/components/responsive-column.txt');
 var standardColumn = require('raw!./../examples/components/standard-column.txt');
 var offsetColumn = require('raw!./../examples/components/offset-column.txt');
 var pushedColumn = require('raw!./../examples/components/pushed-column.txt');
 var nestedColumn = require('raw!./../examples/components/nested-column.txt');
+
+var basicForm = require('raw!./../examples/components/basic-form.txt');
+var inlineForm = require('raw!./../examples/components/inline-form.txt');
+var horizontalForm = require('raw!./../examples/components/horizontal-form.txt');
+
+var buttonOptions = require('raw!./../examples/components/button-options.txt');
+var buttonSizes = require('raw!./../examples/components/button-sizes.txt');
+var buttonStates = require('raw!./../examples/components/button-states.txt');
 
 var MEDIA_QUERIES = {
   sm: '(min-width: 768px)',
@@ -38,6 +44,7 @@ var colDemoStyles = {
   background: 'rgba(117,190,255,0.5)',
   border: '1px solid #0074D9',
   color: '#0074D9',
+  minHeight: 80,
   textAlign: 'center',
   paddingTop: '1em',
   paddingBottom: '1em'
@@ -55,6 +62,7 @@ var App = React.createClass({
   render: function () {
     return (
       <Container breakpoints={this.state.breakpoints}>
+        <h1 style={{borderBottom: '1px solid #eee'}}>Grid system</h1>
         <h2>Fluid container</h2>
         <Example markup={fluidContainer}>
           {convertExample(fluidContainer, this)}
@@ -85,6 +93,7 @@ var App = React.createClass({
           {convertExample(nestedColumn, this)}
         </Example>
 
+        <h1 style={{borderBottom: '1px solid #eee'}}>Forms</h1>
         <h2>Basic form</h2>
         <Example markup={basicForm}>
           {convertExample(basicForm)}
@@ -98,6 +107,22 @@ var App = React.createClass({
         <h2>Horizontal form</h2>
         <Example markup={horizontalForm}>
           {convertExample(horizontalForm, this)}
+        </Example>
+
+        <h1 style={{borderBottom: '1px solid #eee'}}>Buttons</h1>
+        <h2>Button options</h2>
+        <Example markup={buttonOptions}>
+          {convertExample(buttonOptions)}
+        </Example>
+
+        <h2>Button sizes</h2>
+        <Example markup={buttonSizes}>
+          {convertExample(buttonSizes)}
+        </Example>
+
+        <h2>Button states</h2>
+        <Example markup={buttonStates}>
+          {convertExample(buttonStates)}
         </Example>
       </Container>
     );
